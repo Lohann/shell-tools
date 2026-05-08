5ceaf72b69d0927b9057ab8dd987873c3d7f3e70
Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
2026-05-07 21:28:06 -0300

    Fix test_varname zsh pattern matching

--- scripts/test_varname.sh
+++ scripts/test_varname.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
+# This file is part of Shell Tools
+# original https://github.com/Lohann/shell-tools
+# SPDX-License-Identifier: MIT
 
-# valid_varname <STRING>
+# test_varname <VARNAME>
 # ----------------------
-# Check all arguments, check if <STRING> is a valid shell varname
+# Check if <VARNAME> is a valid shell variable name
 test_varname ()
 {
-  test "$#" -gt 0 || return 127
-  while :; do
+  test "$#" -gt 0 || { echo "test_varname: no arguments" >&2; return 127; }
+  while :
+  do
+    # Avoid depending upon Character Ranges.
     # https://www.gnu.org/savannah-checkouts/gnu/autoconf/manual/autoconf-2.72/html_node/Special-Shell-Variables.html
-    case ${1} in
-      [0-9_] | [!a-zA-Z_]* | *[!a-zA-Z0-9_]* ) return 1 ;;
-      [a-zA-Z_]* ) test $# -gt 1 || return 0 ;;
-      * ) return 1 ;;
+    case $1 in
+      _) return 1 ;;
+      [_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]*)
+        LC_ALL=C expr "X${1}" ":" "X.*[^_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]" >/dev/null 2>/dev/null &&
+        return 1 || :
+        ;;
+      *) return 1 ;;
     esac
+    test "$#" -gt 1 || return 0
     shift 2> /dev/null || return 125
   done
-}
+} # test_varname
